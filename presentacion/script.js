de6a972f9117bfc3e5fdf0f817cea1fc72adc90b
João Lizot <premtrabajo@gmail.com>
2023-09-27 11:47:50 -0300

    ...

--- presentacion/script.js
+++ presentacion/script.js
@@ -2,45 +2,66 @@ document.addEventListener("DOMContentLoaded", function () {
     var generatePhotosButton = document.getElementById("generatePhotos");
     var apiDiv = document.getElementById("API");
     var contenedorBotones = document.getElementById('contenedorBotones');
+    var reloadButton = document.createElement("button");
+    reloadButton.textContent = "Recargar Fotos";
+    reloadButton.classList.add("btn");
+    reloadButton.classList.add("btn-light");
+    reloadButton.classList.add("btn-primary");
+    var clearButton = document.createElement("button");
+    clearButton.textContent = "Limpiar";
+    clearButton.classList.add("btn");
+    clearButton.classList.add("btn-light");
+    clearButton.classList.add("btn-secondary");
 
+    var usedImageUrls = [];
+    var maxImages = 15;
+    
     generatePhotosButton.addEventListener("click", function () {
         apiDiv.innerHTML = ''; // Limpiamos el contenido actual del div
+        generatePhotosButton.style.display = "none"; // Ocultar el botón "Generar Fotos"
 
-        // Generar 10 imágenes al azar desde la API de Lorem Picsum
-        for (var i = 0; i < 12; i++) {
+        var generatedUrls = [];
+        
+        for (var i = 0; i < maxImages; i++) {
+            var imageUrl;
+            do {
+                // Genera una URL única para cada imagen
+                imageUrl = "https://picsum.photos/200/200/?random=" + Math.random();
+            } while (usedImageUrls.includes(imageUrl) || generatedUrls.includes(imageUrl)); // Verifica si la URL ya se ha utilizado o generado
+            
+            generatedUrls.push(imageUrl); // Agrega la URL a la lista de imágenes generadas
+
+            var card = document.createElement("div");
+            card.classList.add("card");
             var image = document.createElement("img");
-            image.src = "https://picsum.photos/200/200/?random=" + Math.random();
-            apiDiv.appendChild(image);
+            image.src = imageUrl;
+
+            card.appendChild(image);
+
+            apiDiv.appendChild(card);
         }
 
-        // Agregamos los botones de recargar y limpiar
-        var reloadButton = document.createElement("button");
-        reloadButton.textContent = "Recargar Fotos";
-        reloadButton.classList.add("btn");
-        reloadButton.classList.add("btn-light");
-        reloadButton.classList.add("btn-primary");
-        var clearButton = document.createElement("button");
-        clearButton.textContent = "Limpiar";
-        clearButton.classList.add("btn");
-        clearButton.classList.add("btn-light");
-        clearButton.classList.add("btn-secondary");
+        usedImageUrls = usedImageUrls.concat(generatedUrls);
 
 
+        // Mostrar los botones de recargar y limpiar
         contenedorBotones.appendChild(reloadButton);
         contenedorBotones.appendChild(clearButton);
+    });
 
-        // Manejar la recarga de fotos
-        reloadButton.addEventListener("click", function () {
-            contenedorBotones.innerHTML = '';
-            generatePhotosButton.click();
-        });
+    // Manejar la recarga de fotos
+    reloadButton.addEventListener("click", function () {
+        apiDiv.innerHTML = '';
+        generatePhotosButton.style.display = "none"; // Ocultar el botón "Generar Fotos"
+        reloadButton.style.display = "block"; // Ocultar el botón "Recargar Fotos"
+        clearButton.style.display = "block"; // Ocultar el botón "Limpiar"
+        generatePhotosButton.click();
+    });
 
-        // Manejar la limpieza de la API
-        clearButton.addEventListener("click", function () {
-            apiDiv.innerHTML = '';
-            apiDiv.appendChild(generatePhotosButton);
-            contenedorBotones.innerHTML = '';
-
-        });
+    // Manejar la limpieza de la API
+    clearButton.addEventListener("click", function () {
+        apiDiv.innerHTML = '';
+        contenedorBotones.innerHTML = '';
+        generatePhotosButton.style.display = "block"; // Mostrar el botón "Generar Fotos" nuevamente
     });
 });
